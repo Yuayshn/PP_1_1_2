f6e275185331b37bc70eceae693fde405e327707
Yuayshn <tchumakov.parni-ch@yandex.ru>
2023-08-11 11:39:55 +0300

    Checking changes in the file Main.java and uploading to the github via the terminal

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,7 +2,6 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        // project must be empty (task text)
-        //askdasjdasjdas
+        // project must be empty (task text)gi
     }
 }
